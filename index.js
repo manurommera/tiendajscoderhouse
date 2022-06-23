beb818952fef409bcdbeefc41a0017848d65353b
Manu Simon <manusimon8@gmail.com>
2022-06-23 18:58:27 -0300

    Un pequeño paso para el hombre

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 let nombre = prompt("Ingrese su nombre porfavor.");
-let numeroprod;
+
 function Saludar() {
     alert("Hola " + nombre + ".Bienvenido a su tienda online");
 };
 
 Saludar();
 
+let suma = 0;
 function Tienda() {
-    let suma = 0;
-    while (numeroprod !== 9){
-        prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5->")
-        if (numeroprod == 1){
-            suma = suma+1000;
-            alert("Se agregó Shampoo Sólido a tu carrito")
+    let numeroprod = parseInt(prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5-> Ver total. 9-> Salir."));
+    while (numeroprod != 9){
+        switch(numeroprod){
+            case 1:
+                alert("Se agregó Shampoo Sólido a tu carrito")
+                suma = suma+1000;
+                break;
+            case 2:
+                alert("Se agregó Jabon Natural a tu carrito")
+                suma = suma+500;
+                break;
+            case 3:
+                alert("Se agregó Aceite Natural a tu carrito")
+                suma = suma+2000;
+                break;
+            case 4:
+                alert("Se agregó Vela Aromática a tu carrito")
+                suma = suma+750;
+                break;
+            case 5:
+                alert("Esta es su cuenta " + "$" + suma)
+                break;
+            case 9:
+                alert("Gracias, vuelva prontos")
+                break;
         }
-        if (numeroprod == 2){
-            suma = suma+500;
-            alert("Se agregó Jabon Natural a tu carrito")
-        }
-        if (numeroprod == 3){     
-            suma = suma+2000;
-            alert("Se agregó Aceite Natural a tu carrito")
-        }
-        if (numeroprod == 4){
-            suma = suma+750;
-            alert("Se agregó Vela Aromática a tu carrito")
-        }
-        if (numeroprod == 5){
-            document.write(suma);
-            alert("Esta es su cuenta " + "$" + suma)
-        }
-        if (numeroprod == 9){
-            alert("Gracias, vuelva prontos")
-        }
+        parseInt(prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5-> Ver total. 9-> Salir."));
     }
 }
 
