3aa60c53f089bf466514106324d6ded7ebb8cd42
Manu Simon <manusimon8@gmail.com>
2022-06-23 16:46:46 -0300

    css y ciclo

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-let usuario = prompt("Ingrese el usuario");
-let intento = 0;
+let nombre = prompt("Ingrese su nombre porfavor.");
+let numeroprod;
+function Saludar() {
+    alert("Hola " + nombre + ".Bienvenido a su tienda online");
+};
 
-while (usuario != "manuel") {
-    alert("Usuario incorrecto");
+Saludar();
 
-    if (intento === 3) {
-        break;
+function Tienda() {
+    let suma = 0;
+    while (numeroprod !== 9){
+        prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5->")
+        if (numeroprod == 1){
+            suma = suma+1000;
+            alert("Se agregó Shampoo Sólido a tu carrito")
+        }
+        if (numeroprod == 2){
+            suma = suma+500;
+            alert("Se agregó Jabon Natural a tu carrito")
+        }
+        if (numeroprod == 3){     
+            suma = suma+2000;
+            alert("Se agregó Aceite Natural a tu carrito")
+        }
+        if (numeroprod == 4){
+            suma = suma+750;
+            alert("Se agregó Vela Aromática a tu carrito")
+        }
+        if (numeroprod == 5){
+            document.write(suma);
+            alert("Esta es su cuenta " + "$" + suma)
+        }
+        if (numeroprod == 9){
+            alert("Gracias, vuelva prontos")
+        }
     }
-
-    intento = intento + 1;
-    usuario = prompt("Ingrese el usuario");
 }
 
-if (intento === 3) {
-    alert("Acabaste tus intentos. Prueba mas tarde");
-} else {
-    alert("Ingreso exitoso!");
-}
+Tienda();
+
+
