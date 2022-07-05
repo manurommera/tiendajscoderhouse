c0858b23b2733a093df8ba5d50087bef645ebde6
Manu Simon <manusimon8@gmail.com>
2022-07-04 22:18:51 -0300

    Entrega 1 Proyecto final

--- index.js
+++ index.js
@@ -4,40 +4,58 @@ function Saludar() {
     alert("Hola " + nombre + ".Bienvenido a su tienda online");
 };
 
-Saludar();
 
-let suma = 0;
-function Tienda() {
-    let numeroprod = parseInt(prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5-> Ver total. 9-> Salir."));
-    while (numeroprod != 9){
-        switch(numeroprod){
-            case 1:
-                alert("Se agregó Shampoo Sólido a tu carrito")
-                suma = suma+1000;
-                break;
-            case 2:
-                alert("Se agregó Jabon Natural a tu carrito")
-                suma = suma+500;
-                break;
-            case 3:
-                alert("Se agregó Aceite Natural a tu carrito")
-                suma = suma+2000;
-                break;
-            case 4:
-                alert("Se agregó Vela Aromática a tu carrito")
-                suma = suma+750;
-                break;
-            case 5:
-                alert("Esta es su cuenta " + "$" + suma)
-                break;
-            case 9:
-                alert("Gracias, vuelva prontos")
-                break;
-        }
-        parseInt(prompt("Coloca el numero respectivo del producto: 1-> Shampoo Sólido= $1000 2-> Jabon Natural= $500 3-> Aceite Natural= $2000 4-> Vela Aromática= $750 5-> Ver total. 9-> Salir."));
+Saludar();
+class producto{
+    constructor(numero, nombre, precio){
+        this.numero = numero;
+        this.nombre = nombre;
+        this.precio = precio;
     }
 }
 
+
+function Tienda() {
+    let carrito = [];
+    let numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+    while (numeroprod > 0 && numeroprod < 6){
+        if (numeroprod == 1) {
+                alert("Se agregó Shampoo Sólido a tu carrito");
+                let Shampoo = new producto(1, "Shampoo solido", 1000);
+                carrito.push(Shampoo)
+                numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+        }
+        else if (numeroprod == 2){
+                alert("Se agregó Jabon Natural a tu carrito");
+                let Jabon = new producto(2, "Jabon Natural", 500);
+                carrito.push(Jabon)
+                numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+        }
+        else if (numeroprod == 3){
+                alert("Se agregó Aceite Natural a tu carrito");
+                let Aceite = new producto(3, "Aceite Natural", 2000);
+                carrito.push(Aceite)
+                numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+        }
+        else if (numeroprod == 4){
+                alert("Se agregó Vela Aromática a tu carrito");
+                let Vela = new producto(4, "Vela Aromática", 750);
+                carrito.push(Vela)
+                numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+        }
+        else if (numeroprod == 5){
+                alert("Esta es su cuenta " + "$" + carrito);
+                numeroprod = parseInt(prompt(`Coloca el numero respectivo del producto: \n 1-> Shampoo Sólido= $1000 \n 2-> Jabon Natural= $500 \n 3-> Aceite Natural= $2000 \n 4-> Vela Aromática= $750 \n 5-> Ver total. \n 0-> Salir.`));
+        }
+    }
+    alert("Gracias, vuelva prontos");
+}
+
+
+function suma(productos){
+    const precio = producto.map(prod=>prod.precio)
+}
+
+
+
 Tienda();
-
-
